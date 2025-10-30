ea6428e61f248fac774cebc8d0c1877a7475917c
Cypest <tafeturn@gmail.com>
2025-10-30 13:13:16 -0600

    Test 3

--- app/src/test/java/com/example/lab08/CustomListTest.java
+++ app/src/test/java/com/example/lab08/CustomListTest.java
@@ -11,4 +11,28 @@ public class CustomListTest {
         list.addCity(calgary);
         assertTrue(list.hasCity(calgary));
     }
+
+    @Test
+    void deleteCity_removesExistingCity() {
+        CustomList list = new CustomList();
+        City calgary = new City("Calgary", "AB");
+        City edmonton = new City("Edmonton", "AB");
+        list.addCity(calgary);
+        list.addCity(edmonton);
+
+        list.deleteCity(calgary);
+
+        assertFalse(list.hasCity(calgary));
+        assertTrue(list.hasCity(edmonton));
+    }
+
+    @Test
+    void deleteCity_noop_whenCityNotPresent() {
+        CustomList list = new CustomList();
+        list.addCity(new City("Calgary", "AB"));
+
+        list.deleteCity(new City("Vancouver", "BC"));
+
+        assertEquals(1, list.countCities(), "Deleting absent city shouldn't change size");
+    }
 }
